7826f6de123499b2fc07e9f390f3d4566f262cf1
Laura <law7676@outlook.com>
2023-08-01 13:53:11 -0500

    completed app

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,20 +1,45 @@
-const names = ['John', 'Sally', 'Jane', 'Roberto', 'Felipe', 'Phineas', 'Gigi']
+async function fetchAndAppend(url, id) {
+  return fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      for (let i = 0; i < data.results.length; i++) {
+        let h3 = document.createElement('h3')
+        h3.innerHTML = data.results[i].title
+        if (h3 !== null || h3 !== undefined) {
+          document.getElementById(`${id}`).appendChild(h3)
+        }
+        let img = document.createElement('img')
+        img.src = `https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}`
+        document.getElementById(`${id}`).appendChild(img)
+      }
+    })
+}
 
-console.log(names)
+window.onload = () => {
+  fetchAndAppend(
+    'https://api.themoviedb.org/3/discover/movie?api_key=a116b5ec3c6d5c4300f0c6c2db97c131&with_genres=80',
+    'img-container',
+  )
+  fetchAndAppend(
+    'https://api.themoviedb.org/3/discover/movie?api_key=a116b5ec3c6d5c4300f0c6c2db97c131&with_genres=35',
+    'img-container',
+  )
 
-let randomNum = Math.floor(Math.random() * names.length)
+  fetchAndAppend(
+    'https://api.themoviedb.org/3/discover/movie?api_key=a116b5ec3c6d5c4300f0c6c2db97c131&with_genres=18',
+    'img-container',
+  )
+  fetchAndAppend(
+    'https://api.themoviedb.org/3/discover/movie?api_key=a116b5ec3c6d5c4300f0c6c2db97c131&with_genres=80',
+    'img-crime',
+  )
+  fetchAndAppend(
+    'https://api.themoviedb.org/3/discover/movie?api_key=a116b5ec3c6d5c4300f0c6c2db97c131&with_genres=35',
+    'img-comedies',
+  )
 
-let currentName = names[randomNum]
-
-document.querySelector('.name').append(currentName)
-
-fetch('https://api.imgflip.com/get_memes')
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data.data.memes)
-    for (let i = 0; i < data.data.memes.length; i++) {
-      let img = document.createElement('img')
-      img.src = data.data.memes[i].url
-      document.getElementById('img-container').appendChild(img)
-    }
-  })
+  fetchAndAppend(
+    'https://api.themoviedb.org/3/discover/movie?api_key=a116b5ec3c6d5c4300f0c6c2db97c131&with_genres=18',
+    'img-dramas',
+  )
+}
--- src/_data/data.js
+++ src/_data/data.js
@@ -1,11 +1,61 @@
 const axios = require('axios')
 
-module.export = async function () {
-  // export the result of this
+module.exports = async function () {
+  let movieData = {
+    comedyMovies: [],
+    crimeMovies: [],
+    dramaMovies: [],
+    documentaryMovies: [],
+  }
+
   try {
-    const response = await axios.get('https://api.imgflip.com/get_memes')
-    //console.log(res.data.data)
-    return response.data.data
+    const comedyMovies = await axios.get(
+      'https://api.themoviedb.org/3/discover/movie?api_key=a116b5ec3c6d5c4300f0c6c2db97c131&with_genres=35',
+    )
+
+    for (let i = 0; i < comedyMovies.data.results.length; i++) {
+      let tempMovieObj = {
+        title: comedyMovies.data.results[i].title,
+        poster_path: comedyMovies.data.results[i].poster_path,
+      }
+      movieData.comedyMovies.push(tempMovieObj)
+    }
+
+    const crimeMovies = await axios.get(
+      'https://api.themoviedb.org/3/discover/movie?api_key=a116b5ec3c6d5c4300f0c6c2db97c131&with_genres=80',
+    )
+
+    for (let i = 0; i < crimeMovies.data.results.length; i++) {
+      let tempMovieObj = {
+        title: crimeMovies.data.results[i].title,
+        poster_path: crimeMovies.data.results[i].poster_path,
+      }
+      movieData.crimeMovies.push(tempMovieObj)
+    }
+    const dramaMovies = await axios.get(
+      'https://api.themoviedb.org/3/discover/movie?api_key=a116b5ec3c6d5c4300f0c6c2db97c131&with_genres=18',
+    )
+
+    for (let i = 0; i < dramaMovies.data.results.length; i++) {
+      let tempMovieObj = {
+        title: dramaMovies.data.results[i].title,
+        poster_path: dramaMovies.data.results[i].poster_path,
+      }
+      movieData.dramaMovies.push(tempMovieObj)
+    }
+
+    const documentaryMovies = await axios.get(
+      'https://api.themoviedb.org/3/discover/movie?api_key=a116b5ec3c6d5c4300f0c6c2db97c131&with_genres=99',
+    )
+
+    for (let i = 0; i < documentaryMovies.data.results.length; i++) {
+      let tempMovieObj = {
+        title: documentaryMovies.data.results[i].title,
+        poster_path: documentaryMovies.data.results[i].poster_path,
+      }
+      movieData.documentaryMovies.push(tempMovieObj)
+    }
+    console.log(movieData)
   } catch (error) {
     console.log(error)
   }
